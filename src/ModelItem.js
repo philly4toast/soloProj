8d4d2cd339b4aeb32f32fdcb459df711b7f9ba23
philly4toast <phillipyoun@gmail.com>
2020-05-07 20:49:47 -0700

    Remove ALL errors from console

--- src/ModelItem.js
+++ src/ModelItem.js
@@ -1,16 +1,34 @@
 import React from 'react'
 
 
-function ModelItem(props){
-  return (
-    <div>
-    <div className="models">
-      <input type='checkbox' checked={props.model.completed} id={props.model.id} onChange={()=>props.handleChange(props.model.id)} />
-      <p>{props.model.text}</p>
-    </div>
-      <img className="thumbnail" src={props.model.imageURL}/>
+
+class ModelItem extends React.Component{
+
+  render(){
+    return (
+      <div>
+      <div className="models">
+          <input type='checkbox' checked={this.props.model.completed} id={this.props.model.id} onChange={()=>this.props.handleChange(this.props.model.id)} />
+          <p>{this.props.model.text}</p>
+        </div>
+          <img className="thumbnail" src={this.props.model.imageURL}/>
       </div>
-  )
+    )
+  }
+
 }
+
+
+// function ModelItem(props){
+//   return (
+//     <div>
+//     <div className="models">
+//       <input type='checkbox' checked={props.model.completed} id={props.model.id} onChange={()=>props.handleChange(props.model.id)} />
+//       <p>{props.model.text}</p>
+//     </div>
+//       <img className="thumbnail" src={props.model.imageURL}/>
+//       </div>
+//   )
+// }
 
 export default ModelItem;
