d5d9c6c3d678acb08e09ce5874e7d3710b4ea2d6
philly4toast <phillipyoun@gmail.com>
2020-05-12 15:24:33 -0700

    Added express, how to run?

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.port
+
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'jezebel',
+  database: 'ww2Mod'
+});
+
+connection.connect();
+
+
+app.listen(port, () => {
+  console.log('On port #', port)
+})
